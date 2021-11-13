aba1a07b9df69f996330b636da308f02e67cc978
Guillaume BRANCHE <guillaume.branche@gmail.com>
2021-11-13 13:08:52 +0100

    collisions works yay

--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -6,20 +6,30 @@ public class CharacterController : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
     Vector3 forward, right;
+    CapsuleCollider _playerCollider;
+    Rigidbody _rbPlayer;
+   
     void Start()
     {
         forward = Camera.main.transform.forward;
         forward.y = 0;
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+        _playerCollider = GetComponent<CapsuleCollider>();
+        _rbPlayer = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate()
+    {       
         if (Input.anyKey)
         {
             Move();
+        }
+        else
+        {
+            _rbPlayer.velocity = Vector3.zero;
         }
     }
 
@@ -29,11 +39,11 @@ public class CharacterController : MonoBehaviour
         Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
         Vector3 forwardMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
 
-   
+        _rbPlayer.velocity = rightMovement + forwardMovement;
+       
+            
 
-        
-
-        transform.position += rightMovement;
-        transform.position += forwardMovement;
     }
+
+
 }
