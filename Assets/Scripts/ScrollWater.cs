2278a96e464de27f6d350180248d3b1ea7be803b
Khalil <khalilmeziou@yahoo.fr>
2021-11-14 12:39:26 +0100

    retouche

--- Assets/Scripts/ScrollWater.cs
+++ Assets/Scripts/ScrollWater.cs
@@ -15,7 +15,7 @@ public class ScrollWater : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 offset = new Vector2(Time.time * speed, 0);
+        Vector2 offset = new Vector2(0, Time.time * speed);
 
         GetComponent<MeshRenderer>().material.mainTextureOffset = offset;
     }
